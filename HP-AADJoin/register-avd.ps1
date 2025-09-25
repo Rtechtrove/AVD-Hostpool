8999010ab09500d95d1949d9a742b191172d9873
Rtechtrove <rahmanstechtrove@gmail.com>
2025-09-25 20:22:06 +0530

    Update register-avd.ps1

--- HP-AADJoin/register-avd.ps1
+++ HP-AADJoin/register-avd.ps1
@@ -1,5 +1,4 @@
 # Download AVD Agent and Bootloader
-
 $agentUrl = "https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RWrmXv"
 $bootloaderUrl = "https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RWrxrH"
 
@@ -9,13 +8,8 @@ Invoke-WebRequest -Uri $bootloaderUrl -OutFile "AVDBootloader.msi"
 Start-Process msiexec.exe -ArgumentList "/i AVDAgent.msi /quiet /norestart" -Wait
 Start-Process msiexec.exe -ArgumentList "/i AVDBootloader.msi /quiet /norestart" -Wait
 
-# Inject registration token
-$token = Get-Content ".\registrationToken.txt" | Out-String
-$regPath = "HKLM:\SOFTWARE\Microsoft\RDInfraAgent"
-Set-ItemProperty -Path $regPath -Name "RegistrationToken" -Value $token.Trim()
+# Read token from file
+$token = Get-Content "$PSScriptRoot\registrationToken.txt" | Out-String
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\RDInfraAgent" -Name "RegistrationToken" -Value $token.Trim()
 
 Restart-Service RDAgentBootLoader
-
-
-
-
